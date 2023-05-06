a028816c55ed51f0b0b7b5d9573bca254958da76
saurabh <sauravvermas1097v@gmail.com>
2023-05-07 00:37:45 +0530

    first commit

---
+++ client/script.js
@@ -0,0 +1,41 @@
+console.log("ljfjas");
+
+
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+function loader(element){
+  element.textContent = '';
+
+  loadInterval = setInterval(() =>{
+    element.textContent += '.';
+
+    if(element.textContent === '...'){
+      element.textContent = '';
+    }
+  }, 300)
+
+function typeText(element,text){
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if(index < text.length){
+      element.innerHTML += text;
+    }else{
+      clearInterval(interval);
+    }
+  },20)
+}
+
+function generateUniqueID(){
+  const timeStamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString();
+  
+  reutrn `id-${timeStamp}-${hexadecimalString}`
+}
+
+}
