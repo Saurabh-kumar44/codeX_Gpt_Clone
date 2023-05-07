0548d2fe119a0cff343bd2a7d18f16e71da72a34
saurabh <sauravvermas1097v@gmail.com>
2023-05-07 13:17:02 +0530

    Client-Side complete

--- client/script.js
+++ client/script.js
@@ -1,84 +1,98 @@
-console.log("Welcome Codex");
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
 
+const form = document.querySelector('form')
+const chatContainer = document.querySelector('.chat_container')
 
-import bot from './assets/bot.svg';
-import user from './assets/user.svg';
+let loadInterval
 
-const form = document.querySelector('form');
-const chatContainer = document.querySelector('#chat_container');
-
-//loading functionality 
 function loader(element) {
-  element.textContent = '';
+    element.textContent = ''
 
-  loadInterval = setInterval(() => {
-    element.textContent += '.';
+    loadInterval = setInterval(() => {
+        // Update the text content of the loading indicator
+        element.textContent += '.';
 
-    if (element.textContent === '...') {
-      element.textContent = '';
-    }
-  }, 300)
+        // If the loading indicator has reached three dots, reset it
+        if (element.textContent === '....') {
+            element.textContent = '';
+        }
+    }, 300);
 }
 
-  //typing style
-  function typeText(element, text) {
-    let index = 0;
+function typeText(element, text) {
+    let index = 0
 
     let interval = setInterval(() => {
-      if (index < text.length) {
-        element.innerHTML += text;
-      } else {
-        clearInterval(interval);
-      }
+        if (index < text.length) {
+            element.innerHTML += text.charAt(index)
+            index++
+        } else {
+            clearInterval(interval)
+        }
     }, 20)
-  }
+}
 
-  //generate unique id for text
-  function generateUniqueID() {
-    const timeStamp = Date.now();
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
+function generateUniqueId() {
+    const timestamp = Date.now();
     const randomNumber = Math.random();
-    const hexadecimalString = randomNumber.toString();
+    const hexadecimalString = randomNumber.toString(16);
 
-    reutrn`id-${timeStamp}-${hexadecimalString}`
-  }
+    return `id-${timestamp}-${hexadecimalString}`;
+}
 
-  //dark and light chat Stripe
-  function chatStrip(isAi, value, uniqueId) {
+function chatStripe(isAi, value, uniqueId) {
     return (
-    `
-    <div class="wrapper ${isAi && 'ai'}">
-      <div class="chat">
-        <div class="profile">
-            <img src="${isAi ? bot : user}"
-                 alt="${isAi ? 'bot' : 'user'}"
-            />
+        `
+        <div class="wrapper ${isAi && 'ai'}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? 'bot' : 'user'}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
         </div>
-        <div class="message" id="${uniqueId}"value="${value}"></div>
-      </div>
-    </div>
     `
     )
-  }
+}
 
-  //handling the Submit button
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+const handleSubmit = async (e) => {
+    e.preventDefault()
 
-    const data = FormData(form);
+    const data = new FormData(form)
     // console.log(data);
 
-    //user's ChatStripe
-    chatContainer.innerHTML += chatStrip(false, data.get('promt'))
-    form.reset();
+    // user's chatstripe
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+    console.log(data.get('prompt'));
 
-    //user's ChatStripe
-    const uniqueId = generateUniqueID();
-    chatContainer.innerHTML += chatStrip(true, " ", uniqueId)
+    // to clear the textarea input 
+    form.reset()
 
-    //this is used to put new messages in the view
-    chatContainer.scrollTop = chatContainer.scrollHeight 
+    // bot's chatstripe
+    const uniqueId = generateUniqueId()
+    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
 
-    const messageDiv = document.getElementById(uniqueId); //we have to create a new uniqueId for every new message
+    // to focus scroll to the bottom 
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+    // specific message div 
+    const messageDiv = document.getElementById(uniqueId)
+
+    // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-  }
+}
+
+form.addEventListener('submit', handleSubmit)
+form.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        handleSubmit(e)
+    }
+})
